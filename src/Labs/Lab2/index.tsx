a0952f72de6d0414a03a36b9f471ad8600a30aaa
Mackenzie Creamer <18mcreamer@gmail.com>
2024-10-01 14:11:41 -0400

    Add missed comment from early lab

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -23,7 +23,8 @@ export default function Lab2() {
     <div className="container" id="wd-lab2">
       <h2>Lab 2 - Cascading Style Sheets</h2>
       <h3>Styling with the STYLE attribute</h3>
-      <p>
+      <p /*style={{ backgroundColor: "blue",
+                  color: "white" }}*/>
         Style attribute allows configuring look and feel
         right on the element. Although it's very convenient
         it is considered bad practice and you should avoid
