67451c3823a9aac3cf71f7023aec819579dd9b41
Mackenzie Creamer <18mcreamer@gmail.com>
2024-10-01 16:06:46 -0400

    Finish up to the end of 4.4, with the exception of week 2 in 4.4

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -4,111 +4,127 @@ export default function Dashboard() {
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
       <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
-      <div id="wd-dashboard-courses">
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/1234/Home">
-            <img src="/images/React.png" width={200} />
-            <div>
-              <h5>
-                 CS1234 React JS
-              </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer
-              </p>
-              <button> Go </button>
+      <div id="wd-dashboard-courses" className="row">
+        <div className="row row-cols-1 row-cols-md-5 g-4">
+          <div className="wd-dashboard-course col">
+            <div className="card rounded-3 overflow-hidden">
+              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                    to="/Kanbas/Courses/1234/Home">
+                <img src="/images/React.png"  width="100%" height={160} />
+                <div className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">
+                    CS1234 React JS
+                  </h5>
+                  <p className="wd-dashboard-course-title card-text">
+                    Full Stack software developer
+                  </p>
+                  <button className="btn btn-primary"> Go </button>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/1234/Home">
-            <img src="/images/FGInsignia.png" width={200} />
-            <div>
-              <h5>
-                 CS1212 Digital Art
-              </h5>
-              <p className="wd-dashboard-course-title">
-                Personal Digital Art Creation 
-              </p>
-              <button> Go </button>
+          </div>
+          <div className="wd-dashboard-course col">
+            <div className="card rounded-3 overflow-hidden">
+              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                    to="/Kanbas/Courses/1234/Home">
+                <img src="/images/FGInsignia.png" width="100%" height={160} />
+                <div className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">
+                    CS1212 Digital Art
+                  </h5>
+                  <p className="wd-dashboard-course-title card-text">
+                    Personal Digital Art Creation 
+                  </p>
+                  <button className="btn btn-primary"> Go </button>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/1234/Home">
-            <img src="/images/MechaSkinkObject.png" width={200} />
-            <div>
-              <h5>
-                 CS2212 Digital Art II
-              </h5>
-              <p className="wd-dashboard-course-title">
-                Personal Digital Art Creation II
-              </p>
-              <button> Go </button>
+          </div>
+          <div className="wd-dashboard-course col">
+            <div className="card rounded-3 overflow-hidden">
+              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                    to="/Kanbas/Courses/1234/Home">
+                <img src="/images/MechaSkinkObject.png"  width="100%" height={160} />
+                <div className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">
+                    CS2212 Digital Art II
+                  </h5>
+                  <p className="wd-dashboard-course-title card-text">
+                    Personal Digital Art Creation II
+                  </p>
+                  <button className="btn btn-primary"> Go </button>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/1234/Home">
-            <img src="/images/smoothBrain.png" width={200} />
-            <div>
-              <h5>
-                CS3212 Digital Art III
-              </h5>
-              <p className="wd-dashboard-course-title">
-                Personal Digital Art Creation III
-              </p>
-              <button> Go </button>
+          </div>
+          <div className="wd-dashboard-course col">
+            <div className="card rounded-3 overflow-hidden">
+              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                    to="/Kanbas/Courses/1234/Home">
+                <img src="/images/smoothBrain.png" width="100%" height={160} />
+                <div className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">
+                    CS3212 Digital Art III
+                  </h5>
+                  <p className="wd-dashboard-course-title card-text">
+                    Personal Digital Art Creation III
+                  </p>
+                  <button className="btn btn-primary"> Go </button>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/1234/Home">
-            <img src="/images/html.png" width={200} />
-            <div>
-              <h5>
-                 CS1232 HTML 
-              </h5>
-              <p className="wd-dashboard-course-title">
-                Web Development (HTML Only)
-              </p>
-              <button> Go </button>
+          </div>
+          <div className="wd-dashboard-course col">
+            <div className="card rounded-3 overflow-hidden">
+              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                    to="/Kanbas/Courses/1234/Home">
+                <img src="/images/html.png" width="100%" height={160} />
+                <div className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">
+                    CS1232 HTML 
+                  </h5>
+                  <p className="wd-dashboard-course-title card-text">
+                    Web Development (HTML Only)
+                  </p>
+                  <button className="btn btn-primary"> Go </button>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/1234/Home">
-            <img src="/images/wallp-js.png" width={200} />
-            <div>
-              <h5>
-                 CS1233 JS
-              </h5>
-              <p className="wd-dashboard-course-title">
-                Javascript Development (only JS)
-              </p>
-              <button> Go </button>
+          </div>
+          <div className="wd-dashboard-course col">
+            <div className="card rounded-3 overflow-hidden">
+              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                    to="/Kanbas/Courses/1234/Home">
+                <img src="/images/wallp-js.png" width="100%" height={160} />
+                <div className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">
+                    CS1233 JS
+                  </h5>
+                  <p className="wd-dashboard-course-title card-text">
+                    Javascript Development (only JS)
+                  </p>
+                  <button className="btn btn-primary"> Go </button>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/1234/Home">
-            <img src="/images/Rust.png" width={200} />
-            <div>
-              <h5>
-                 CS4234 Rust
-              </h5>
-              <p className="wd-dashboard-course-title">
-                Rust Development Course
-              </p>
-              <button> Go </button>
+          </div>
+          <div className="wd-dashboard-course col">
+            <div className="card rounded-3 overflow-hidden">
+              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                    to="/Kanbas/Courses/1234/Home">
+                <img src="/images/Rust.png" width="100%" height={160} />
+                <div className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">
+                    CS4234 Rust
+                  </h5>
+                  <p className="wd-dashboard-course-title card-text">
+                    Rust Development Course
+                  </p>
+                  <button className="btn btn-primary"> Go </button>
+                </div>
+              </Link>
             </div>
-          </Link>
+          </div>
         </div>
       </div>
     </div>
