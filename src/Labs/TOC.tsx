3b688c5bfdd3f18523ebb8099c68e19d848f78f3
Mackenzie Creamer <18mcreamer@gmail.com>
2024-10-03 10:58:01 -0400

    Update github link

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -49,7 +49,7 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-k" href="https://github.com/jannunzi" className="nav-link">
+        <a id="wd-github" href="https://github.com/MackenzieCreamer/" className="nav-link">
           My GitHub
         </a>
       </li>
