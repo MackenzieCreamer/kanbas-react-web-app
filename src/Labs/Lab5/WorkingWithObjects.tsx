3d009d117ada4451c75716da39267260ab82e225
MackenzieCreamer <creamer.m@northeastern.edu>
2024-11-08 16:47:02 -0500

    Finish up to the end of 3.3.4

--- src/Labs/Lab5/WorkingWithObjects.tsx
+++ src/Labs/Lab5/WorkingWithObjects.tsx
@@ -6,7 +6,13 @@ export default function WorkingWithObjects() {
         description: "Create a NodeJS server with ExpressJS",
         due: "2021-10-10", completed: false, score: 0,
     });
+    const [module, setModule] = useState({
+        id: "1", name: "Web Development",
+        description: "Learn how to use modern frameworks in web applications.",
+        course: "CS5001"
+    });
     const ASSIGNMENT_API_URL = `${REMOTE_SERVER}/lab5/assignment`
+    const MODULE_API_URL = `${REMOTE_SERVER}/lab5/module`
     return (
         <div id="wd-working-with-objects">
         <h3>Working With Objects</h3>
@@ -19,6 +25,29 @@ export default function WorkingWithObjects() {
         <input className="form-control w-75" id="wd-assignment-title"
             defaultValue={assignment.title} onChange={(e) =>
             setAssignment({ ...assignment, title: e.target.value })}/>
+        <br />
+        <hr />
+        <a id="wd-update-assignment-score"
+            className="btn btn-primary float-end"
+            href={`${ASSIGNMENT_API_URL}/score/${assignment.score}`}>
+            Update Score
+        </a>
+        <input type="number" className="form-control w-75" id="wd-assignment-score"
+            defaultValue={assignment.score} onChange={(e) =>
+            setAssignment({ ...assignment, score: parseInt(e.target.value) })}/>
+        <hr />
+        <br />
+        <a id="wd-update-assignment-completed"
+            className="btn btn-primary float-end"
+            href={`${ASSIGNMENT_API_URL}/completed/${assignment.completed}`}>
+            Update Completed
+        </a>
+        <div className="form-group">
+            <label htmlFor="wd-assignment-completed">Completed</label>
+            <input className="ms-1" type="checkbox" id="wd-assignment-completed"
+            defaultChecked={assignment.completed} onChange={(e) =>
+            setAssignment({ ...assignment, completed: e.target.checked })}/>
+        </div>
         <hr />
 
         <h4>Retrieving Objects</h4>
@@ -30,6 +59,37 @@ export default function WorkingWithObjects() {
         <a id="wd-retrieve-assignment-title" className="btn btn-primary"
             href={`${REMOTE_SERVER}/lab5/assignment/title`}>
             Get Title
+        </a><hr/>
+        <h3>On your own (Modules)</h3>
+        <h4>Modifying Properties</h4>
+        <a id="wd-update-module-name"
+            className="btn btn-primary float-end"
+            href={`${MODULE_API_URL}/name/${module.name}`}>
+            Update Module Name
+        </a>
+        <input className="form-control w-75" id="wd-module-name"
+            defaultValue={module.name} onChange={(e) =>
+            setModule({ ...module, name: e.target.value })}/>
+        <br /><hr />
+        <a id="wd-update-module-description"
+            className="btn btn-primary float-end"
+            href={`${MODULE_API_URL}/description/${module.description}`}>
+            Update Module Description
+        </a>
+        <input className="form-control w-75" id="wd-module-description"
+            defaultValue={module.description} onChange={(e) =>
+            setModule({ ...module, description: e.target.value })}/>
+        <hr />
+
+        <h4>Retrieving Objects</h4>
+        <a id="wd-retrieve-modules" className="btn btn-primary"
+            href={`${MODULE_API_URL}`}>
+            Get Module
+        </a><hr/>
+        <h4>Retrieving Properties</h4>
+        <a id="wd-retrieve-module-name" className="btn btn-primary"
+            href={`${MODULE_API_URL}/name`}>
+            Get Module Name
         </a><hr/>
         </div>
     )
