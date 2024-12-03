f4c5a2fbfe0ceb3efc2be5adeb78ec149749eeb6
Mackenzie Creamer <18mcreamer@gmail.com>
2024-12-03 12:29:01 -0500

    Finish up to the end of section 5.2

--- src/Kanbas/Courses/EnrollmentProtection.tsx
+++ src/Kanbas/Courses/EnrollmentProtection.tsx
@@ -1,7 +1,6 @@
 import { useSelector } from "react-redux";
 import { Navigate, useParams } from "react-router-dom";
 export default function EnrollmentProtection({ children, enrolledCourses }: { children: any, enrolledCourses : any[] }) {
-  console.log(children)
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const { cid } = useParams();
   if (currentUser.role === "FACULTY" || enrolledCourses.some((compareCourse) => {return (compareCourse._id === cid)})) {
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -81,10 +81,7 @@ export default function Kanbas() {
     const updateCourseLists = async () => {
         fetchCourses();
         fetchAllCourses();
-    }
-
-    console.log(courses)
-  
+    }  
     return (
         <Session>
             <div id="wd-kanbas">
