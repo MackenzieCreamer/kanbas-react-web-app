f7c7d70b402d1b62d7423e89c62c8df599d438f0
Mackenzie Creamer <18mcreamer@gmail.com>
2024-12-04 09:29:35 -0500

    Finish up to the end of 5.3 as a whole

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -7,12 +7,24 @@ import AssignmentEditor from "./Assignments/Editor";
 import { FaAlignJustify } from "react-icons/fa";
 import { Navigate, Route, Routes, useParams, useLocation} from "react-router";
 import PeopleTable from "./People/Table";
+import { useEffect, useState } from "react";
+import * as client from "./client"
+
 
 export default function Courses({ courses }: { courses: any[]; }) {
     const { cid } = useParams();
     const course = courses.find((course) => course._id === cid);
     const { pathname } = useLocation();
-    console.log(course)
+
+    const [users, setUsers] = useState<any[]>([]);
+    const fetchUsers = async () => {
+      const users = await client.findUsersForCourse(course._id);
+      setUsers(users);
+    };
+    useEffect(() => {
+      fetchUsers();
+    }, [course._id]);
+
     return (
       <div id="wd-courses">
         <h2 className="text-danger">
@@ -31,7 +43,7 @@ export default function Courses({ courses }: { courses: any[]; }) {
               <Route path="Modules" element={<Modules />} />
               <Route path="Assignments" element={<Assignments />} />
               <Route path="Assignments/:aid" element={<AssignmentEditor />} />
-              <Route path="People" element={<PeopleTable />} />
+              <Route path="People" element={<PeopleTable users={users} />} />
               </Routes>
           </div>
         </div>
